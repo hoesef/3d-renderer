1fe9eaef5479aba39e5e7f61780fa5a5b9b4b419
Joe <joe.luke.manning@gmail.com>
2025-05-06 21:14:19 +0100

    Added a parser class for parsing .obj files, along with an abstract class to make it easier to add capability to parse different file types (stl, fbx, etc)

---
+++ include/parser/object-parser.h
@@ -0,0 +1,28 @@
+#ifndef OBJECT_PARSER_H_
+#define OBJECT_PARSER_H_
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+
+#include "../../include/objects/polymesh.h"
+
+/*
+ * Object parser abstract base class
+ * 
+ * Derrived classes allow for the parsing of 3d models stored as different file types
+ */
+class ObjectParser {
+
+    public:
+        // Constructor
+        ObjectParser() = default;
+        // File parser
+        virtual Polymesh* parse(const char* filename) = 0;
+        // Destructor
+        virtual ~ObjectParser() = default;
+
+};
+
+#endif
