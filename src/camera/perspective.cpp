b7bc9f75ddb090ae3f33bd7aebadfac1f1e485be
Joe <joe.luke.manning@gmail.com>
2025-05-08 18:50:25 +0100

    Added a base camera interface that has a render function, added a perspective camera class that is derived from the camera base class and responsible for doing perspective projection rendering

---
+++ src/camera/perspective.cpp
@@ -0,0 +1,87 @@
+#include "..\..\include\camera\perspective.h"
+#include <math.h>
+#include "..\..\include\framebuffer\linedrawer.h"
+
+// Check if math variables have been defined
+#ifndef M_PI
+#define M_PI 3.1415926535f
+#endif
+
+void homogenize(Vertex& v) {
+    if (v.m_w != 1 && v.m_w != 0) {
+        v.m_x = v.m_x / v.m_w;
+        v.m_y = v.m_y / v.m_w;
+        v.m_z = v.m_z / v.m_w;
+        v.m_w = v.m_w / v.m_w;
+    }
+}
+
+float max(float a, float b) {
+    return a*(a > b) + b*(b > a);
+}
+float min(float a, float b) {
+    return a*(a < b) + b*(b < a);
+}
+
+Perspective::Perspective() { makeProjMatrix(); }
+Perspective::Perspective(int width, int height, float fov) {
+    m_width = width;
+    m_height = height;
+    m_a = (float)height / (float)width;
+    m_fov = fov;
+    makeProjMatrix();
+}
+void Perspective::render(Polymesh& mesh, Framebuffer& fb) {
+    for (unsigned int i = 0; i < mesh.m_tri_count; i++) {
+        Vertex v0 = mesh.m_vertices[mesh.m_tris[i].v0];
+        Vertex v1 = mesh.m_vertices[mesh.m_tris[i].v1];
+        Vertex v2 = mesh.m_vertices[mesh.m_tris[i].v2];
+
+        v0.m_z += 11.0f;
+        v1.m_z += 11.0f;
+        v2.m_z += 11.0f;
+
+        // if w!=1 and w!=0, divide by w
+        homogenize(v0);
+        homogenize(v1);
+        homogenize(v2);
+
+        v0 = m_proj * v0;
+        v1 = m_proj * v1;
+        v2 = m_proj * v2;
+
+        // w=z, so divide by w (perspective divide)
+        homogenize(v0);
+        homogenize(v1);
+        homogenize(v2);        
+
+        // Convert to NCD, then scale to actual image
+        float scaleX = 0.5f * m_width;
+        float scaleY = 0.5f * m_height;
+        v0.m_x = (v0.m_x + 1.0f) * scaleX; v0.m_y = (1.0f - v1.m_y) * scaleY;
+        v1.m_x = (v1.m_x + 1.0f) * scaleX; v1.m_y = (1.0f - v1.m_y) * scaleY;
+        v2.m_x = (v2.m_x + 1.0f) * scaleX; v2.m_y = (1.0f - v2.m_y) * scaleY;
+
+        // Clamping
+        v0.m_x = min(max(0.0f, v0.m_x), (float)m_width); v0.m_y = min(max(0.0f, v0.m_y), (float)m_height);
+        v1.m_x = min(max(0.0f, v1.m_x), (float)m_width); v1.m_y = min(max(0.0f, v1.m_y), (float)m_height);
+        v2.m_x = min(max(0.0f, v2.m_x), (float)m_width); v2.m_y = min(max(0.0f, v2.m_y), (float)m_height);
+
+        // Drawline
+        drawline(fb, (uint32_t)v0.m_x, (uint32_t)v0.m_y, (uint32_t)v1.m_x, (uint32_t)v1.m_y);
+        drawline(fb, (uint32_t)v0.m_x, (uint32_t)v0.m_y, (uint32_t)v2.m_x, (uint32_t)v2.m_y);
+        drawline(fb, (uint32_t)v1.m_x, (uint32_t)v1.m_y, (uint32_t)v2.m_x, (uint32_t)v2.m_y);
+        
+    }
+}
+void Perspective::makeProjMatrix() {
+    float f = 1 / tanf(m_fov * 0.5f * M_PI / 180);
+    float q = m_zFar / (m_zFar - m_zNear);
+
+    m_proj = Matrix4x4(
+        m_a * f, 0, 0, 0,
+        0, f, 0, 0,
+        0, 0, q, 1,
+        0, 0, -m_zNear * q, 0);
+}
+Perspective::~Perspective() {}
