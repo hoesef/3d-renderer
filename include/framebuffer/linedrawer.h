ac1d7a48b76c947b366b85044df28b5563e51c70
Joe <joe.luke.manning@gmail.com>
2025-05-11 14:19:05 +0100

    added a drawTriangle and fillTriangle funciton to the linedrawer function

--- include/framebuffer/linedrawer.h
+++ include/framebuffer/linedrawer.h
@@ -4,8 +4,16 @@
 #include <cstdint>
 #include "../../include/framebuffer/framebuffer.h"
 
+class Vertex;
+
 void drawline(Framebuffer& fb, uint32_t x0, uint32_t y0, uint32_t x1, uint32_t y1);
 void drawlineH(Framebuffer& fb, uint32_t x0, uint32_t y0, uint32_t x1, uint32_t y1);
 void drawlineV(Framebuffer& fb, uint32_t x0, uint32_t y0, uint32_t x1, uint32_t y1);
+
+// Draw Triangle
+// void drawTriangle(Framebuffer& fb, float x0, float y0, float x1, float y1, float x2, float y2);
+void drawTriangle(Framebuffer& fb, Vertex& v0, Vertex& v1, Vertex& v2);
+void fillTriangle(Framebuffer& fb, Vertex& v0, Vertex& v1, Vertex& v2);
+bool pointInTriangle(Vertex& A, Vertex& B, Vertex& C, Vertex& P);
 
 #endif
