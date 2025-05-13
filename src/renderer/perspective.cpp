a8c7fb0006b97bb41f8f4a03f2312a1ff7e38c8c
Joe <joe.luke.manning@gmail.com>
2025-05-13 21:32:03 +0100

    Bug fixes: - Fixed bug in framebuffer, was only checking for max and min red values, now checking for all r,g,b channels. - Corrected the program to now use a right handed coordinate system. - Perspective::render now uses the transformed face normal for lighting and rendering, correcly handling changes in scale.

--- src/renderer/perspective.cpp
+++ src/renderer/perspective.cpp
@@ -43,25 +43,36 @@ void Perspective::render(Polymesh& mesh, Framebuffer& fb) {
         makeProjMatrix();
     }
 
+    Matrix4x4 R = mesh.transform.getRotation();
+    Matrix4x4 S = mesh.transform.getScale();
+    Matrix4x4 M = R * S;
+    M.m_mat[3] = M.m_mat[7] = M.m_mat[11] = 0;
+    M.m_mat[12] = M.m_mat[13] = M.m_mat[14] = 0;
+    M.m_mat[15] = 0;
+    Matrix4x4 normalMatrix;
+    M.inverse(normalMatrix);
+    normalMatrix.transpose();
+
     for (unsigned int i = 0; i < mesh.m_tri_count; i++) {
         // Get vertex data
         Vertex v0 = mesh.m_vertices[mesh.m_tris[i].v0];
         Vertex v1 = mesh.m_vertices[mesh.m_tris[i].v1];
         Vertex v2 = mesh.m_vertices[mesh.m_tris[i].v2];
 
+        // Get surface normal
+        Vector normal, line1, line2;
+        line1 = v1 - v0;
+        line2 = v2 - v0;
+        normal = line1.cross(line2);
+        // normal.normalize();
+        normal = normalMatrix * normal;
+        normal.normalize();
+
         // Position vertex (temporary, will be moved to object.applyTransform)
         Matrix4x4 transform = mesh.transform.get();
         v0 = transform * v0;
         v1 = transform * v1;
         v2 = transform * v2;
-
-        // Get surface normal
-        Vector normal, line1, line2;
-        line1 = v1 - v0;
-        line2 = v2 - v0;
-
-        normal = line1.cross(line2);
-        normal.normalize();
         
         // Check if face is towards camera
         if (normal.dot(v0.toVector() - pos) >= 0) { continue; }
@@ -70,6 +81,10 @@ void Perspective::render(Polymesh& mesh, Framebuffer& fb) {
         v0 = m_proj * v0;
         v1 = m_proj * v1;
         v2 = m_proj * v2;
+
+        if (v0.m_w <= 0 || v1.m_w <= 0 || v2.m_w <= 0) {continue;}
+
+        // std::cout << "v0: " << v0 << "\nv1: " << v1 << "\nv2 " << v2 << "\n\n";
 
         // w=z, so divide by w (perspective divide)
         homogenize(v0);
@@ -90,7 +105,8 @@ void Perspective::render(Polymesh& mesh, Framebuffer& fb) {
 
         // Drawline
         // Very basic temporary lighting
-        float x = normal.dot({0, 0, -1});
+        float x = normal.dot({0, 0, 1});
+        std::cout << "x: " << x << "\n\n";
         Colour c = mesh.getColour();
         c.red *= x; c.green *= x; c.blue *= x;
         fillTriangle(fb, v0, v1, v2, c);
@@ -103,6 +119,6 @@ void Perspective::makeProjMatrix() {
         f * m_a, 0, 0, 0,
               0, f, 0, 0,
               0, 0, q, (-m_zNear)*q,
-              0, 0, 1, 0);
+              0, 0, -1, 0);
 }
 Perspective::~Perspective() {}
